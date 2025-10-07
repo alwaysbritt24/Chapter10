d5642e122cdd9f29e73609c33c5a6b5fcd5ccb10
alwaysbritt24 <bslee05@icloud.com>
2025-10-07 15:35:49 -0400

    Initial Commit

---
+++ Lootlogger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lootlogger
+//
+//  Created by Brittany on 10/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LootloggerTests/LootloggerTests.swift
@@ -0,0 +1,17 @@
+//
+//  LootloggerTests.swift
+//  LootloggerTests
+//
+//  Created by Brittany on 10/7/25.
+//
+
+import Testing
+@testable import Lootlogger
+
+struct LootloggerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
