3eed89f228a976bd93cf7c42a2ba910c61a29111
alwaysbritt24 <bslee05@icloud.com>
2025-10-07 16:33:16 -0400

    integrate code and made edits to table controller

--- Lootlogger/ItemStore.swift
+++ Lootlogger/ItemStore.swift
@@ -5,4 +5,27 @@
 //  Created by Brittany on 10/7/25.
 //
 
-import Foundation
+import UIKit
+
+class ItemStore {
+
+    var allItems = [Item]()
+
+    @discardableResult func createItem() -> Item {
+        let newItem = Item(random: true)
+
+        allItems.append(newItem)
+
+        return newItem
+    }
+    
+    init() {
+        for _ in 0..<5 {
+            createItem()
+        }
+    }
+   
+}
+
+
+
--- Lootlogger/ItemsViewController.swift
+++ Lootlogger/ItemsViewController.swift
@@ -5,4 +5,45 @@
 //  Created by Brittany on 10/7/25.
 //
 
-import Foundation
+import UIKit
+
+class ItemsViewController: UITableViewController {
+    
+    var itemStore: ItemStore!
+    
+    override func tableView(_ tableView: UITableView,
+                            numberOfRowsInSection section: Int) -> Int {
+        return itemStore.allItems.count
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                            cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Get a new or recycled cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell",
+                                                 for: indexPath)
+        
+        // Set the text on the cell with the description of the item
+        // that is at the nth index of items, where n = row this cell
+        // will appear in on the table view
+        let item = itemStore.allItems[indexPath.row]
+        
+        cell.textLabel?.text = item.name
+        cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+        
+        return cell
+    }
+    
+    class ItemsViewController: UITableViewController {
+        
+        var itemStore: ItemStore!
+        
+        @IBAction func addNewItem(_ sender: UIButton) {
+            
+        }
+        
+        @IBAction func toggleEditingMode(_ sender: UIButton) {
+            
+        }
+    }
+}
+
