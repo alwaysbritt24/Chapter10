13e68697b58f46ee3e731e9a871395facc5817df
alwaysbritt24 <bslee05@icloud.com>
2025-10-19 11:03:03 -0400

    CH9

--- Lootlogger/ItemsViewController.swift
+++ Lootlogger/ItemsViewController.swift
@@ -11,6 +11,36 @@ class ItemsViewController: UITableViewController {
     
     var itemStore: ItemStore!
     
+    @IBAction func addNewItem(_ sender: UIButton) {
+        
+        let newItem = itemStore.createItem()
+
+           // Figure out where that item is in the array
+           if let index = itemStore.allItems.firstIndex(of: newItem) {
+               let indexPath = IndexPath(row: index, section: 0)
+
+               // Insert this new row into the table
+               tableView.insertRows(at: [indexPath], with: .automatic)
+           }
+    }
+    
+    @IBAction func toggleEditingMode(_ sender: UIButton) {
+        
+        if isEditing {
+                // Change text of button to inform user of state
+                sender.setTitle("Edit", for: .normal)
+
+                // Turn off editing mode
+                setEditing(false, animated: true)
+            } else {
+                // Change text of button to inform user of state
+                sender.setTitle("Done", for: .normal)
+
+                // Enter editing mode
+                setEditing(true, animated: true)
+            }
+    }
+    
     override func tableView(_ tableView: UITableView,
                             numberOfRowsInSection section: Int) -> Int {
         return itemStore.allItems.count
@@ -33,17 +63,29 @@ class ItemsViewController: UITableViewController {
         return cell
     }
     
-    class ItemsViewController: UITableViewController {
-        
-        var itemStore: ItemStore!
-        
-        @IBAction func addNewItem(_ sender: UIButton) {
-            
-        }
-        
-        @IBAction func toggleEditingMode(_ sender: UIButton) {
-            
+    override func tableView(_ tableView: UITableView,
+                            commit editingStyle: UITableViewCell.EditingStyle,
+                            forRowAt indexPath: IndexPath) {
+        // If the table view is asking to commit a delete command...
+        if editingStyle == .delete {
+            let item = itemStore.allItems[indexPath.row]
+
+            // Remove the item from the store
+            itemStore.removeItem(item)
+
+            // Also remove that row from the table view with an animation
+            tableView.deleteRows(at: [indexPath], with: .automatic)
         }
     }
+
+    override func tableView(_ tableView: UITableView,
+                            moveRowAt sourceIndexPath: IndexPath,
+                            to destinationIndexPath: IndexPath) {
+        // Update the model
+        itemStore.moveItem(from: sourceIndexPath.row, to: destinationIndexPath.row)
+    }
+    
+
+        
 }
 
